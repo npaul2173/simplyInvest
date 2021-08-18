86a1dbc270afce30aa6626ec029707cbd3123bb4
Nabendu Paul <npaul2173@gmail.com>
2021-08-18 20:38:11 +0530

    StatButton component created and added to home page

---
+++ src/common/components/StatsContainer/index.js
@@ -0,0 +1,11 @@
+import "./index.styles.css";
+
+// Stats Container
+export const StatsContainer = () => {
+  return (
+    <div className="statsWrapper">
+      <div className="statContainer">2300+</div>
+      <div className="statTitle">Participants</div>
+    </div>
+  );
+};
--- src/home/index.js
+++ src/home/index.js
@@ -1,67 +1,78 @@
 import React from "react";
 import { InfoBox } from "../common/components/InfoBox";
+import { StatsContainer } from "../common/components/StatsContainer";
 import "./index.styles.css";
 export const Home = () => {
   const newsArray = new Array(3).fill({});
   const announcements = new Array(7).fill({});
   return (
     <div className="homeContainer">
-      <div className="rightContainer">
-        <div className="headerTitle">
-          <span className="headers">MF News</span>
+      <div className="homeFirstContainer">
+        <div className="rightContainer">
+          <div className="headerTitle">
+            <span className="headers">MF News</span>
+          </div>
+
+          <div className="infowrapper">
+            {newsArray.map(() => {
+              return (
+                <InfoBox
+                  dataEntity={{
+                    title: "Market manners",
+                    description: "link to description",
+                  }}
+                />
+              );
+            })}
+          </div>
+
+          <div className="gapwrapper"></div>
+
+          {/* Expert views section */}
+          <div className="headerTitle">
+            <span className="headers">Expert views</span>
+          </div>
+
+          <div className="infowrapper">
+            {newsArray.map(() => {
+              return (
+                <InfoBox
+                  dataEntity={{
+                    title: "Market manners",
+                    description: "link to description",
+                  }}
+                />
+              );
+            })}
+          </div>
         </div>
 
-        <div className="infowrapper">
-          {newsArray.map(() => {
-            return (
-              <InfoBox
-                dataEntity={{
-                  title: "Market manners",
-                  description: "link to description",
-                }}
-              />
-            );
-          })}
-        </div>
+        <div className="leftContainer">
+          <div className="headerTitle">
+            <span className="headers">Important announcements</span>
+          </div>
 
-        <div className="gapwrapper"></div>
-
-        {/* Expert views section */}
-        <div className="headerTitle">
-          <span className="headers">Expert views</span>
-        </div>
-
-        <div className="infowrapper">
-          {newsArray.map(() => {
-            return (
-              <InfoBox
-                dataEntity={{
-                  title: "Market manners",
-                  description: "link to description",
-                }}
-              />
-            );
-          })}
+          <div className="infowrapper">
+            {announcements.map(() => {
+              return (
+                <InfoBox
+                  dataEntity={{
+                    title: "Market manners",
+                    description: "link to description",
+                  }}
+                />
+              );
+            })}
+          </div>
         </div>
       </div>
 
-      <div className="leftContainer">
-        <div className="headerTitle">
-          <span className="headers">Important announcements</span>
-        </div>
-
-        <div className="infowrapper">
-          {announcements.map(() => {
-            return (
-              <InfoBox
-                dataEntity={{
-                  title: "Market manners",
-                  description: "link to description",
-                }}
-              />
-            );
-          })}
-        </div>
+      {/* Stats */}
+      <div className="statsSectionWrapper">
+        <StatsContainer />
+        <StatsContainer />
+        <StatsContainer />
+        <StatsContainer />
       </div>
     </div>
   );
