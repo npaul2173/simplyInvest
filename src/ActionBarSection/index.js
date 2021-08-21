e723d4f4e6103422df1bb15163131142cc8f175f
Nabendu Paul <npaul2173@gmail.com>
2021-08-21 15:44:45 +0530

    Warning removed from action button

--- src/ActionBarSection/index.js
+++ src/ActionBarSection/index.js
@@ -1,16 +1,16 @@
 import {
-  ActionButton_1,
-  ActionButton_2,
-  ActionButton_3,
+  ActionButtonFatca,
+  ActionButtonKYC,
+  ActionButtonSupplementary,
 } from "../common/components/ActionButton";
 import "./index.styles.css";
 
 export const ActionBarSection = () => {
   return (
     <div className="actionBarWrapper">
-      <ActionButton_1 />
-      <ActionButton_2 />
-      <ActionButton_3 />
+      <ActionButtonKYC />
+      <ActionButtonFatca />
+      <ActionButtonSupplementary />
     </div>
   );
 };
