35b863fe0860820d0571ad9140be4951fb7b4158
Nabendu Paul <npaul2173@gmail.com>
2021-09-07 08:44:56 +0530

    Download Button Responsiveness Done

--- src/common/components/DownloadButton/index.js
+++ src/common/components/DownloadButton/index.js
@@ -4,16 +4,17 @@ import { AiOutlineCloudDownload } from "react-icons/ai";
 
 export const DownloadButton = () => {
   return (
-    <div class={styles.downloadBtnContainer}>
-      <a
-        rel="noreferrer"
-        href="https://www.hq.nasa.gov/alsj/a17/A17_FlightPlan.pdf"
-        target="_blank"
-        download
-      >
-        <AiOutlineCloudDownload className={styles.downloadIcon} />
-        <span> Download Brochure</span>
-      </a>
-    </div>
+    // <div className={styles.container}>
+    <a
+      rel="noreferrer"
+      href="https://www.hq.nasa.gov/alsj/a17/A17_FlightPlan.pdf"
+      target="_blank"
+      download
+      className={styles.downloadBtnContainer}
+    >
+      <AiOutlineCloudDownload className={styles.downloadIcon} />
+      <span> Download Brochure</span>
+    </a>
+    // </div>
   );
 };
