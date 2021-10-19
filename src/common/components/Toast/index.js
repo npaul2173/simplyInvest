724ca037db26a35d086f3eee83232af086416e82
Nabendu Paul <npaul2173@gmail.com>
2021-10-19 17:11:52 +0530

    Toast COmponent updated

--- src/common/components/Toast/index.js
+++ src/common/components/Toast/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./index.modules.css";
+import { BsCheck } from "react-icons/bs";
+
 export const ToastMessage = ({ message }) => {
   return (
     <div id="toast">
-      <div id="img">Icon</div>
+      <div id="img">
+        <BsCheck style={{ fontSize: 30 }} />
+      </div>
       <div id="desc">A notification message..</div>
     </div>
   );
